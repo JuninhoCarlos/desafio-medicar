3a3a36f7fce829a73d5955921de48ed2fee94646
Juninho <juninho.ufpi@hotmail.com>
2021-04-14 17:20:22 -0300

    Implementados os modelos da aplicacao e com as duas restricoes exigidas: i) Nao deve ser possivel criar mais de uma agenda para um medico em um mesmo dia e ii) Nao deve ser possivel criar uma agenda para um medico em um dia passado.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Especialidade, Medico, Horario, Agenda
 
 # Register your models here.
+admin.site.register(Especialidade)
+admin.site.register(Medico)
+admin.site.register(Horario)
+admin.site.register(Agenda)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import date
 
-# Create your models here.
+
+class Especialidade(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Medico(models.Model):
+    nome = models.CharField(max_length=100)
+    crm = models.CharField(max_length=15)
+    email = models.EmailField(blank=True)
+    telefone = models.CharField(max_length=15, blank=True)
+    especialidade = models.ForeignKey(Especialidade,
+                                      blank=True,
+                                      null=True,
+                                      on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
+
+class Horario(models.Model):
+    horario = models.TimeField(unique=True)
+
+    def __str__(self):
+        return str(self.horario.strftime("%H:%M:%S"))
+
+
+class Agenda(models.Model):
+    medico = models.ForeignKey(Medico,
+                               unique_for_date="dia",
+                               on_delete=models.CASCADE)
+    dia = models.DateField()
+    horarios = models.ManyToManyField(Horario)
+
+    def __str__(self):
+        return self.medico.nome + " - " + str(self.dia.strftime("%d/%m/%Y"))
+
+    def clean(self):
+        if self.dia < date.today():
+            raise ValidationError('The agenda should be contain a valid'
+                                  ' date (from today onwards)')
