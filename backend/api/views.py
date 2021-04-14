7556596c1e5b546e0add3e8e85d694a58c3c8169
Juninho <juninho.ufpi@hotmail.com>
2021-04-14 18:56:43 -0300

    Adicionado gitignore. Atualizado os requirements. Implementado a listagem e filtragem de especialidade.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets, filters
 
-# Create your views here.
+from .serializers import EspecialidadeSerializer
+from .models import Especialidade
+
+
+class EspecialidadeView(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = Especialidade.objects.all()
+    serializer_class = EspecialidadeSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['^nome']
