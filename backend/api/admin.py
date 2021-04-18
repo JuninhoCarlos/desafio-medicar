0b94430d46731e2afebc0b3e0147a8fa9d484e7f
juninho <juninho.ufpi@hotmail.com>
2021-04-18 16:50:55 -0300

    Implementados ModelAdmin para melhorar a visualizacao dos dados no django admin e permitir a filtragem pelo django admin.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,19 +2,22 @@ from django.contrib import admin
 
 from .models import Agenda, Especialidade, Horario, Medico
 
-# Register your models here.
-admin.site.register(Especialidade)
-admin.site.register(Medico)
-admin.site.register(Horario)
-
 
 class AgendaAdmin(admin.ModelAdmin):
-    list_display = ("medico", "dia", "get_horarios")
-    list_filter = ("medico", "dia")
+    list_display = ["pk", "medico", "dia", "get_horarios"]
+    list_filter = ["medico", "dia"]
 
     @admin.display(description="Horários")
     def get_horarios(self, obj):
         return ", ".join(i.isoformat() for i in obj.horarios.values_list("horario", flat=True))
 
 
+class MedicoAdmin(admin.ModelAdmin):
+    list_display = ["pk", "nome", "crm", "especialidade"]
+    list_filter = ["nome", "especialidade"]
+
+
+admin.site.register(Medico, MedicoAdmin)
 admin.site.register(Agenda, AgendaAdmin)
+admin.site.register(Especialidade)
+admin.site.register(Horario)
