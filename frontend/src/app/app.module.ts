ebe6d12ff66b13e8edc69eef013812bcfbd08c30
juninho <juninho.ufpi@hotmail.com>
2021-05-05 17:53:36 -0300

    Implementado as telas de login e de cadastro de usuario.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroUsuarioComponent } from './components/registro-usuario/registro-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     BrowserModule,
