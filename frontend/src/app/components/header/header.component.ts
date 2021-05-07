74a7cd6e2ed50886aef6e675f1f91eecf1c6ef34
juninho <juninho.ufpi@hotmail.com>
2021-05-07 12:11:09 -0300

    Implementadas as funcionalidades do header (logout e ler o nome do usuario logado)

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  usuario: string | null;
+  constructor(private authService: AuthService, private router: Router) {
+    this.usuario = localStorage.getItem('usuario');
   }
 
+  ngOnInit(): void {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
 }
