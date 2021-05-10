0f81050a94abfd9fac17283f1baf0e8dd58f3641
juninho <juninho.ufpi@hotmail.com>
2021-05-10 14:50:50 -0300

    Finalizado a implementacao de todas a funcionalidades. Faltam ajustes na UI e mostrar mensagem de sucesso e tratar os erros mais comuns.

--- frontend/src/app/components/lista-consultas/lista-consultas.component.ts
+++ frontend/src/app/components/lista-consultas/lista-consultas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api';
+
 @Component({
   selector: 'app-lista-consultas',
   templateUrl: './lista-consultas.component.html',
@@ -7,11 +10,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaConsultasComponent implements OnInit {
   showModal: boolean;
-  constructor() {
+  form: FormGroup;
+
+  especialidades!: Array<any>;
+  medicos!: Array<any>;
+  datas!: Array<string>;
+  horarios!: Array<string>;
+
+  consultas!: Array<any>;
+
+  private agendaId!: Number;
+
+  constructor(private fb: FormBuilder, private api: ApiService) {
+    this.form = this.fb.group({
+      especialidade: [null, Validators.required],
+      medico: [null, Validators.required],
+      data: [null, Validators.required],
+      hora: [null, Validators.required],
+    });
     this.showModal = false;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.api.getEspecialidades().subscribe((data) => {
+      this.especialidades = data;
+    });
+
+    this.api.getConsultas().subscribe((data) => {
+      console.log(data);
+      this.consultas = data;
+    });
+  }
+
+  onEspecialidadeChange(idEspecialidade: string) {
+    this.api.getMedicosPorEspecialidade(idEspecialidade).subscribe((data) => {
+      this.medicos = data;
+    });
+  }
+
+  onMedicoChange(idMedico: string) {
+    this.api.getAgendasDoMedico(idMedico).subscribe((response) => {
+      this.datas = response.map((agenda) => {
+        return agenda.dia;
+      });
+    });
+  }
+
+  onDataChange(data: string) {
+    this.api
+      .getAgendaDoDiaDoMedico(this.form.get('medico')?.value, data)
+      .subscribe((data) => {
+        data.forEach((agenda: any) => {
+          console.log('horarios desse dia', agenda.horarios);
+          this.agendaId = agenda.id;
+          this.horarios = agenda.horarios;
+        });
+      });
+  }
+
+  desmarcarConsulta(consultaId: Number) {
+    console.log('desmarcar consulta', consultaId);
+    this.api.desmarcarConsulta(consultaId).subscribe((data) => {
+      this.consultas = this.consultas.filter((consulta) => {
+        return consulta.id != consultaId;
+      });
+    });
+  }
 
   toggleShowModal() {
     this.showModal = !this.showModal;
@@ -19,5 +83,17 @@ export class ListaConsultasComponent implements OnInit {
 
   onClose() {
     this.showModal = false;
+  }
+
+  onSubmit() {
+    //console.log('Agenda id: ', this.agendaId);
+    //console.log('horario', this.form.get('hora')?.value);
+    this.api
+      .cadastraConsulta(this.agendaId, this.form.get('hora')?.value)
+      .subscribe((data) => {
+        this.consultas.push(data);
+        this.form.reset();
+        this.showModal = false;
+      });
   }
 }
--- frontend/src/app/services/api/api.service.ts
+++ frontend/src/app/services/api/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
@@ -26,11 +26,75 @@ export class ApiService {
 
   constructor(private http: HttpClient) {}
 
+  private getHeader(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    const header = new HttpHeaders({ Authorization: `Token ${token}` });
+    return header;
+  }
+
   login(usuario: UsuarioAuth): Observable<Token> {
     return this.http.post<Token>(`${this.baseUrl}/login/`, usuario);
   }
 
   cadastraUsuario(usuario: Usuario): Observable<Usuario> {
     return this.http.post<Usuario>(`${this.baseUrl}/users/`, usuario);
+  }
+
+  getEspecialidades(): Observable<Array<object>> {
+    const header = this.getHeader();
+    return this.http.get<Array<object>>(`${this.baseUrl}/especialidades/`, {
+      headers: header,
+    });
+  }
+
+  getMedicosPorEspecialidade(
+    especialidadeId: string
+  ): Observable<Array<object>> {
+    return this.http.get<Array<object>>(
+      `${this.baseUrl}/medicos/?especialidade=${especialidadeId}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+  }
+
+  getAgendasDoMedico(medicoID: string): Observable<Array<any>> {
+    return this.http.get<Array<any>>(
+      `${this.baseUrl}/agendas/?medico=${medicoID}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+  }
+
+  getAgendaDoDiaDoMedico(medicoID: string, dia: string): Observable<any> {
+    return this.http.get<any>(
+      `${this.baseUrl}/agendas/?medico=${medicoID}&data_inicio=${dia}&data_final=${dia}`,
+      {
+        headers: this.getHeader(),
+      }
+    );
+  }
+
+  cadastraConsulta(agendaId: Number, horario: string): Observable<any> {
+    let data = {
+      agenda_id: agendaId,
+      horario: horario,
+    };
+    return this.http.post<any>(`${this.baseUrl}/consultas/`, data, {
+      headers: this.getHeader(),
+    });
+  }
+
+  getConsultas() {
+    return this.http.get<any>(`${this.baseUrl}/consultas/`, {
+      headers: this.getHeader(),
+    });
+  }
+
+  desmarcarConsulta(consultaId: Number) {
+    return this.http.delete<any>(`${this.baseUrl}/consultas/${consultaId}/`, {
+      headers: this.getHeader(),
+    });
   }
 }
